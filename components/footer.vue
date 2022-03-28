5b75f2cd5c4f7100553ed3bb8a4e3c35e87972ee
Dynamit-Harry <neo.fors@abbgymnasiet.se>
2022-03-28 10:22:36 +0200

    instagram link updated

--- components/footer.vue
+++ components/footer.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="bg-indigo-500 bottom-0" style="position: fixed; bottom: 0; width: 100vw;">
     <div class="flex content-center justify-center object-center ">
-    <a href="https://www.instagram.com/nf.spets_project_210s/">
+    <a href="https://www.instagram.com/Neo_f_projekt/">
       <img src="../static/instagram.png" class="w-16 h-16 flex inline-flex object-center">
     </a>
     <a href="">
