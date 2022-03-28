8ed41268313f7d27be79ad3be574973754fb210c
Dynamit-Harry <neo.fors@abbgymnasiet.se>
2022-03-28 08:17:17 +0200

    updated footer link removed unnessesary md files

--- components/DishListItem.vue
+++ components/DishListItem.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="text-center object-center">
     <img :src="item.image" alt="destination image" class="h-80 w-64 object-cover" >
-    <h3>{{item.title}}</h3>
+    <h3 class="relative right-8">{{item.title}}</h3>
     <p>{{item.description}}</p>
 </div>
 </template>
--- pages/about-us.vue
+++ pages/about-us.vue
@@ -1,5 +1,6 @@
 <template>
 <div class="min-h-screen bg-indigo-400 text-center">
 <p class="inline-flex relative right-4 text-lg">Hi! I´m Neo, the creator of this page. <br> I made this page as a school project, our task was to create a blog while trying to keep the pattern of another website <br> in my case i decided i was going to do a travel blog and use the website ribhaus.com as a guide <br> </p>
+<p class="inline-flex relative right-4 text-lg"></p>
 </div>
 </template>
