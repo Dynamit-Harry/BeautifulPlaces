d97bbf11cbe2207cad20dc8589d863c51e0676c7
Dynamit-Harry <neo.fors@abbgymnasiet.se>
2022-04-22 09:54:24 +0200

    changed how far the border of the photo reached

--- components/DishListHome.vue
+++ components/DishListHome.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="text-center object-center mb-24">
+<div class="text-center object-center mb-12">
   <h3 class="text-2xl text-center flex inline-flex">{{item.title}}</h3>
   <img :src="item.image" alt="destination image" class="h-80 w-64 object-cover object-center flex inline-flex" >
 </div>
